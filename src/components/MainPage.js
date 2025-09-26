7af9671af6b4353c7c66a2a479cf320828c1b727
junheeee8462 <jjangjun1001@naver.com>
2025-09-26 15:44:34 +0900

    update main page text

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,12 +11,12 @@ function MainPage() {
       
       <section className="main-hero-section">
         <div className="main-text">
-          <p>'기술 혁신 . 품질 우선 . 사람중심' 의 가치를 바탕으로</p>
+          <p><strong>'기술 혁신 · 품질 우선 · 사람중심'</strong> 의 가치를 바탕으로</p>
           <p>운동기구의 새로운 기준을 제시하는 기업</p>
         </div>
         <div className="second-text">
           <p>피트니스 기구 및 시스템 전문 제조 기업</p>
-          <p>주식회사 어시스타 입니다. </p>
+          <p><strong>주식회사 어시스타</strong> 입니다. </p>
         </div>
       </section>
 
