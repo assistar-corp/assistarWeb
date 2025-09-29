a2ba7780b8d5776651a6cc3a28d33f55796b0adc
junheeee8462 <jjangjun1001@naver.com>
2025-09-29 14:17:50 +0900

    mainpage update

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import './css/MainPage.css';
+import mainPageLogo from '../assests/images/mainpage_logo.svg';
 
 import PartnerLogos from './PartnerLogos';
 
@@ -10,14 +11,8 @@ function MainPage() {
       <Header /> 
       
       <section className="main-hero-section">
-        <div className="main-text">
-          <p><strong>'기술 혁신 · 품질 우선 · 사람중심'</strong> 의 가치를 바탕으로</p>
-          <p>운동기구의 새로운 기준을 제시하는 기업</p>
-        </div>
-        <div className="second-text">
-          <p>피트니스 기구 및 시스템 전문 제조 기업</p>
-          <p><strong>주식회사 어시스타</strong> 입니다. </p>
-        </div>
+        <img src={mainPageLogo} alt="Main Page Logo" className="main-page-logo" />
+        <p className='main-text'>피트니스 라이프의 새로운 가치를 만듭니다.</p>
       </section>
 
       <PartnerLogos />
