a054296eee4cf9c52f18ef98e9c8172d62a0fbc9
junheeee8462 <jjangjun1001@naver.com>
2025-10-08 14:59:34 +0900

    deploy update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import IntroPage from './components/IntroPage';
 import MainPage from './components/MainPage';
 import IntroducePage from './components/IntroducePage';
